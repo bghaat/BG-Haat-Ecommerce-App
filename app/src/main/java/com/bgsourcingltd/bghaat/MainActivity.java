9e710aa9dbd6f0b0b206fd8e27ae9fed787af7fc
bghaat <bghaatapp@gamil.com>
2021-06-30 16:39:46 +0600

    Home Page UI Design

--- app/src/main/java/com/bgsourcingltd/bghaat/MainActivity.java
+++ app/src/main/java/com/bgsourcingltd/bghaat/MainActivity.java
@@ -1,8 +1,13 @@
 package com.bgsourcingltd.bghaat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setBottomNavigation();
+    }
+
+    private void setBottomNavigation() {
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
+        NavController navController = Navigation.findNavController(this,R.id.fragmentContainerView);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
     }
 }
